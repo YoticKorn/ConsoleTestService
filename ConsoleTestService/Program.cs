b3e83c41899cb8d3ddf9ac0d70ff623b15921282
Yotic <yoticcitoy@gmail.com>
2025-04-03 21:18:29 +0300

    Added tests for pipes and server/client sides

--- ConsoleTestService/Program.cs
+++ ConsoleTestService/Program.cs
@@ -1,1 +1,47 @@
-﻿
+﻿using Test.ClientPackets;
+using Test.ServerPackets;
+using Korn.Service;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        var configuration = new ServerConfiguration("TestServer", new PacketList("Test.ClientPackets"), new PacketList("Test.ServerPackets"));
+     
+        /* Server */
+        var server = new Server(configuration);
+        server
+        .Register<PingPacket>((connection, packet) =>
+        {
+            var pong = new PongPacket();
+            connection.Callback(packet, pong);
+        })
+        .Register<LogWritePacket>((connection, packet) => Console.WriteLine(packet.Text));
+
+        /* Client */
+        var client = new Client(configuration);
+        client.Send(new PingPacket(), callback => Console.WriteLine("ping! pong!"));
+
+        for (var i = 0; i < 10000; i++)
+            client.Send(new LogWritePacket($"{i}"));
+
+        Console.ReadLine();
+    }
+}
+
+namespace Test.ClientPackets
+{
+    class PingPacket : ClientCallbackablePacket<PongPacket> { }
+
+    class LogWritePacket : ClientPacket
+    {
+        public LogWritePacket(string text) => Text = text;
+        public string Text;
+    }
+}
+
+namespace Test.ServerPackets
+{
+    class PongPacket : ServerCallbackPacket { }
+}
---
+++ ConsoleTestService/TestPipes.cs
@@ -0,0 +1,39 @@
+﻿using Korn.Pipes;
+using System;
+using System.Threading.Tasks;
+
+static class TestPipes
+{
+    public static void Execute()
+    {
+        var configuration = new PipeConfiguration("pipes_test");
+
+        Task.Run(Task1);
+        Task.Run(Task2);
+        Task.Delay(100000).Wait();
+
+        void Task1()
+        {
+            var output = new OutputPipe(configuration);
+
+            Task.Delay(100000).Wait();
+        }
+
+        void Task2()
+        {
+            using (var input = new InputPipe(configuration))
+            {
+                for (var i = 1; i < 10; i++)
+                {
+                    var bytes = new byte[i];
+                    input.Send(bytes);
+                    Task.Delay(10).Wait();
+                }
+
+                Console.ReadLine();
+            }
+
+            Task.Run(Task2);
+        }
+    }
+}
